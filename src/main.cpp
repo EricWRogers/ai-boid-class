405fa283920847a78c50f43a8a246c09c0071440
EricWRogers <ericwilliamrogers@gmail.com>
2023-11-30 14:09:20 -0600

    SDL Thread

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <windows.h>
 #endif
 
+#include <SDL.h>
+
 #include <Canis/App.hpp>
 #include <Canis/ECS/Decode.hpp>
 #include <Canis/Canis.hpp>
@@ -21,8 +23,34 @@
 #include "ECS/Systems/GameOfLifeSystem.hpp"
 #include "ECS/Systems/BoidSystem.hpp"
 
+#include <iostream>
+
+int ThreadFunction(void* data)
+{
+    for (int i = 0; i < 100; i++)
+    {
+        std::cout << "Thread: " << *(int*)data << " " << i << std::endl;
+    }
+
+    return 0;
+}
+
 int main(int argc, char* argv[])
 {
+    int t1Number = 1;
+    SDL_Thread *t1 = SDL_CreateThread(ThreadFunction, "ThreadFunction", &t1Number); 
+
+    int t2Number = 2;
+    SDL_Thread *t2 = SDL_CreateThread(ThreadFunction, "ThreadFunction", &t2Number); 
+
+    SDL_WaitThread(t1, NULL);
+    SDL_WaitThread(t2, NULL);
+
+    int mainProcessNumber = 0;
+    ThreadFunction(&mainProcessNumber);
+
+    return 0;
+
     Canis::App app;
 
     // decode system
